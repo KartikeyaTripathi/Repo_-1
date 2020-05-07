395c2471b1fe0a592cb7ca0afcc5bf1ef38249ff
Kartikeya <tiwari.lok@gmail.com>
2020-05-07 15:37:45 +0530

    second file

---
+++ dev.java
@@ -0,0 +1,21 @@
+import java.util.*;
+package kartik;
+
+
+public class dev
+{
+	static int a;
+	int b;
+	int c;
+public static void main(String args[])
+{
+	System.out.println("my name is kartikeya");
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter any number");
+	a=sc.nextInt();
+	System.out.println(a*a);
+	
+}
+}
+
+
